9317c2ccfdc4749aeb34fc61f2e7a45cd978eec6
misrianas19 <anasmisri02@gmail.com>
2025-11-29 04:18:05 +0530

    Add full website with styles and scripts

---
+++ script.js
@@ -0,0 +1,5 @@
+﻿const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+});
