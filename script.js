7c34375a2137a15c85af17a0dc20a1d1e08b1f2b
misrianas19 <anasmisri02@gmail.com>
2025-12-01 03:24:39 +0530

    Updated full website with new styles and scripts

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-﻿const burger = document.querySelector('.burger');
+﻿// Burger menu toggle
+const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
+
 burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
 });
